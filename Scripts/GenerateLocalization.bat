0a79073b9ceccdc598e9f23fc7a126111f740f76
Viktor <vityokvit@gmail.com>
2026-08-02 09:25:23 +0200

    Add requirement warning

--- Scripts/GenerateLocalization.bat
+++ Scripts/GenerateLocalization.bat
@@ -1,4 +1,6 @@
 @echo off
+echo Required for this script: .NET 10 SDK (https://dotnet.microsoft.com/en-us/download)
+echo:
 dotnet run GenerateLocalization.cs 0 ../Data/strings.ini > ../Localization/Debug/Data/strings.ini
 echo Generated debug localization strings
 dotnet run GenerateLocalization.cs 1 ../Data/strings.ini > ../Localization.bb
